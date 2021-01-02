557babf8357d2a00498708c00c7a84348d5ea736
DaniilRudyak <45008863+DaniilRudyak@users.noreply.github.com>
2021-01-02 06:16:26 +0300

    Add Jdbc Template

--- src/main/java/ru/leti/project/dao/InfoStudDAO.java
+++ src/main/java/ru/leti/project/dao/InfoStudDAO.java
@@ -1,155 +1,78 @@
 package ru.leti.project.dao;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.leti.project.config.ConfigConnection;
+import ru.leti.project.models.Group;
 import ru.leti.project.models.Student;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Component
 public class InfoStudDAO {
     Connection connection = ConfigConnection.connection;
+    private final JdbcTemplate jdbcTemplate;
+
+    public InfoStudDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
 
     public List<Student> index(int id) {
-        List<Student> students = new ArrayList<>();
+        Group group = jdbcTemplate.query("SELECT * FROM enum_of_groups WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Group.class))
+                .stream().findAny().orElse(null);
 
-        try {
-
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM enum_of_groups WHERE id=?");
-
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            resultSet.next();
-
-            PreparedStatement preparedStatement1 = connection.prepareStatement("SELECT * FROM list_all_student WHERE number_group = ? AND beg_stud = ? AND end_stud = ?");
-
-            preparedStatement1.setInt(1, resultSet.getInt("number_group"));
-            preparedStatement1.setDate(2, resultSet.getDate("beg_stud"));
-            preparedStatement1.setDate(3, resultSet.getDate("end_stud"));
-            ;
-            ResultSet resultSet1 = preparedStatement1.executeQuery();
-
-            while (resultSet1.next()) {
-                Student student = new Student();
-
-                student.setId(resultSet1.getInt("id"));
-                student.setFullName(resultSet1.getString("fullname"));
-                student.setNumberGroup(resultSet1.getInt("number_group"));
-                student.setNumberStudentCard(resultSet1.getInt("number_student_card"));
-                student.setBegStud(resultSet1.getDate("beg_stud"));
-                student.setEndStud(resultSet1.getDate("end_stud"));
-
-                students.add(student);
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return students;
+        return jdbcTemplate.query("SELECT * FROM list_all_student WHERE number_group = ? AND beg_stud = ? AND end_stud = ?"
+                , new Object[]{group.getNumberGroup(), group.getBegStud(), group.getEndStud()}, new BeanPropertyRowMapper<>(Student.class));
     }
 
     public void delete(int id, int number) {
+        Group group = jdbcTemplate.query("SELECT * FROM enum_of_groups WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Group.class))
+                .stream().findAny().orElse(null);
 
+        String fullname = jdbcTemplate.query("SELECT fullname FROM list_all_student WHERE number_student_card = ? AND number_group = ? AND beg_stud = ? AND end_stud = ?"
+                , new Object[]{number, group.getNumberGroup(), group.getBegStud(), group.getEndStud()}, new BeanPropertyRowMapper<>(String.class))
+                .stream().findAny().orElse(null);
 
-        try {
-            String SQL = "SELECT * FROM enum_of_groups WHERE id = " + id;
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SQL);
-            resultSet.next();
+        jdbcTemplate.update("DELETE FROM list_all_student WHERE fullname = ? AND number_group = ? AND beg_stud = ? AND end_stud = ? AND number_student_card = ?",
+                fullname, group.getNumberGroup(), group.getBegStud(), group.getEndStud(), number);
 
-            String SQL1 = "SELECT fullname FROM list_all_student WHERE number_student_card = " + number
-                    + " AND " + "number_group = " + resultSet.getInt("number_group")
-                    + " AND " + "beg_stud = " + "'"+resultSet.getDate("beg_stud")+"'"
-                    + " AND " + "end_stud = " + "'"+resultSet.getDate("end_stud")+"'";
-
-            Statement statement1 = connection.createStatement();
-            ResultSet resultSet1 = statement1.executeQuery(SQL1);
-            resultSet1.next();
-
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("DELETE FROM list_all_student WHERE fullname = ? AND number_group = ? AND beg_stud = ? AND end_stud = ? AND number_student_card = ?");
-            PreparedStatement preparedStatement1 =
-                    connection.prepareStatement("DELETE FROM grade_sheet WHERE fullname = ? AND number_group = ? AND year_of_certification >= ? AND year_of_certification <= ? AND number_student_card = ?");
-
-
-            preparedStatement.setString(1, resultSet1.getString("fullname"));
-            preparedStatement.setInt(2, resultSet.getInt("number_group"));
-            preparedStatement.setDate(3, resultSet.getDate("beg_stud"));
-            preparedStatement.setDate(4, resultSet.getDate("end_stud"));
-            preparedStatement.setInt(5, number);
-            preparedStatement.executeUpdate();
-
-            preparedStatement1.setString(1, resultSet1.getString("fullname"));
-            preparedStatement1.setInt(2, resultSet.getInt("number_group"));
-            preparedStatement1.setDate(3, resultSet.getDate("beg_stud"));
-            preparedStatement1.setDate(4, resultSet.getDate("end_stud"));
-            preparedStatement1.setInt(5, number);
-            preparedStatement1.executeUpdate();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
+        jdbcTemplate.update("DELETE FROM grade_sheet WHERE fullname = ? AND number_group = ? AND year_of_certification >= ? AND year_of_certification <= ? AND number_student_card = ?",
+                fullname, group.getNumberGroup(), group.getBegStud(), group.getEndStud(), number);
     }
 
     public int save(Student student, int id) {
-        try {
-            String SQL = "SELECT * FROM enum_of_groups WHERE id = " + id;
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SQL);
-            resultSet.next();
 
-            PreparedStatement checkPreparedStatement =
-                    connection.prepareStatement("SELECT * FROM list_all_student WHERE number_group = ? AND number_student_card = ? AND beg_stud = ? AND end_stud = ?");
+        Group group = jdbcTemplate.query("SELECT * FROM enum_of_groups WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Group.class))
+                .stream().findAny().orElse(null);
 
-            checkPreparedStatement.setInt(1, resultSet.getInt("number_group"));
-            checkPreparedStatement.setInt(2, student.getNumberStudentCard());
-            checkPreparedStatement.setDate(3, resultSet.getDate("beg_stud"));
-            checkPreparedStatement.setDate(4, resultSet.getDate("end_stud"));
-            ResultSet checkResultSet = checkPreparedStatement.executeQuery();
-            if (checkResultSet.next())
-                return 1;
+        Student studentCheck = jdbcTemplate.query("SELECT * FROM list_all_student WHERE number_group = ? AND number_student_card = ? AND beg_stud = ? AND end_stud = ?",
+                new Object[]{group.getNumberGroup(), student.getNumberStudentCard(), group.getBegStud(), group.getEndStud()}, new BeanPropertyRowMapper<>(Student.class))
+                .stream().findAny().orElse(null);
 
+        if (studentCheck != null)
+            return 1;
 
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO list_all_student(fullname, number_group, number_student_card , beg_stud, end_stud) VALUES(?, ?, ?, ?, ?)");
+        jdbcTemplate.update("INSERT INTO list_all_student(fullname, number_group, number_student_card , beg_stud, end_stud) VALUES(?, ?, ?, ?, ?)"
+                , student.getFullName(), group.getNumberGroup(), student.getNumberStudentCard(), group.getBegStud(), group.getEndStud());
 
+        List<Map<String, Object>> listHelper = jdbcTemplate.queryForList("SELECT course,year_of_study FROM list_all_teacher WHERE teaching_group_number = ? AND year_of_study >= ? AND year_of_study <= ?"
+                , group.getNumberGroup(), group.getBegStud(), group.getEndStud());
 
-            preparedStatement.setString(1, student.getFullName());
-            preparedStatement.setInt(2, resultSet.getInt("number_group"));
-            preparedStatement.setInt(3, student.getNumberStudentCard());
-            preparedStatement.setDate(4, resultSet.getDate("beg_stud"));
-            preparedStatement.setDate(5, resultSet.getDate("end_stud"));
-            preparedStatement.executeUpdate();
-
-            PreparedStatement preparedStatement1 =
-                    connection.prepareStatement("SELECT course,year_of_study FROM list_all_teacher WHERE teaching_group_number = ? AND year_of_study >= ? AND year_of_study <= ?");
-
-
-            preparedStatement1.setInt(1, resultSet.getInt("number_group"));
-            preparedStatement1.setDate(2, resultSet.getDate("beg_stud"));
-            preparedStatement1.setDate(3, resultSet.getDate("end_stud"));
-            ResultSet resultSet1 = preparedStatement1.executeQuery();
-            while (resultSet1.next()) {
-
-                PreparedStatement preparedStatement2 =
-                        connection.prepareStatement("INSERT INTO grade_sheet(fullname, number_group, course, year_of_certification, number_student_card ) VALUES(?, ?, ?, ?, ?)");
-                preparedStatement2.setString(1, student.getFullName());
-                preparedStatement2.setInt(2, resultSet.getInt("number_group"));
-                preparedStatement2.setString(3, resultSet1.getString("course"));
-                preparedStatement2.setDate(4, resultSet1.getDate("year_of_study"));
-                preparedStatement2.setInt(5, student.getNumberStudentCard());
-
-                preparedStatement2.executeUpdate();
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        for (Map<String, Object> cur : listHelper) {
+            Set<String> keySet = cur.keySet();
+            for (String key : keySet)
+                jdbcTemplate.update("INSERT INTO grade_sheet(fullname, number_group, course, year_of_certification, number_student_card ) VALUES(?, ?, ?, ?, ?)"
+                        , student.getFullName(), group.getNumberGroup(), key, ((Integer) (cur.get(key))).intValue(), student.getNumberStudentCard());
         }
         return 0;
     }
-
 }
--- src/main/java/ru/leti/project/dao/InfoStudGroupDAO.java
+++ src/main/java/ru/leti/project/dao/InfoStudGroupDAO.java
@@ -1,7 +1,9 @@
 package ru.leti.project.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import ru.leti.project.config.ConfigConnection;
 import ru.leti.project.models.Group;
 
 import java.sql.*;
@@ -11,96 +13,42 @@ import java.util.List;
 @Component
 public class InfoStudGroupDAO {
 
+    private final JdbcTemplate jdbcTemplate;
 
-    Connection connection = ConfigConnection.connection;
-
-    public List<Group> index() {
-        List<Group> groups = new ArrayList<>();
-
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM enum_of_groups";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while (resultSet.next()) {
-                Group group = new Group();
-
-                group.setId(resultSet.getInt("id"));
-                group.setNumberGroup(resultSet.getInt("number_group"));
-                group.setBegStud(resultSet.getDate("beg_stud"));
-                group.setEndStud(resultSet.getDate("end_stud"));
-
-                groups.add(group);
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return groups;
+    @Autowired
+    public InfoStudGroupDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    public List<Group> index() {
+        return jdbcTemplate.query("SELECT * FROM enum_of_groups", new BeanPropertyRowMapper<>(Group.class));
+    }
 
     public int save(Group group) {
-        try {
-            if (group.getBegStud().after(group.getEndStud())) {
-                return 1;
-            }
 
+        if (group.getBegStud().after(group.getEndStud()))
+            return 1;
 
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO enum_of_groups(number_group , beg_stud, end_stud) VALUES(?, ?, ?)");
+        jdbcTemplate.update("INSERT INTO enum_of_groups(number_group , beg_stud, end_stud) VALUES(?, ?, ?)"
+                , group.getNumberGroup(), group.getBegStud(), group.getEndStud());
 
-            preparedStatement.setInt(1, group.getNumberGroup());
-            preparedStatement.setDate(2, group.getBegStud());
-            preparedStatement.setDate(3, group.getEndStud());
-
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
         return 0;
     }
 
-
     public void delete(int id) {
+        Group group = jdbcTemplate.query("SELECT * FROM enum_of_groups WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Group.class))
+                .stream().findAny().orElse(null);
 
+        jdbcTemplate.update("DELETE FROM list_all_teacher WHERE teaching_group_number = ? AND year_of_study >= ? AND year_of_study <= ?"
+                ,group.getNumberGroup(),group.getBegStud(),group.getEndStud());
 
-        try {
-            PreparedStatement preparedStatement1 = connection.prepareStatement("SELECT * FROM enum_of_groups WHERE id=?");
-            PreparedStatement preparedStatement2 = connection.prepareStatement("DELETE FROM enum_of_groups WHERE id=?");
-            PreparedStatement preparedStatement3 = connection.prepareStatement("DELETE FROM grade_sheet WHERE number_group = ? AND year_of_certification >= ? AND year_of_certification <= ?");
-            PreparedStatement preparedStatement4 = connection.prepareStatement("DELETE FROM list_all_student WHERE number_group = ? AND beg_stud = ? AND end_stud = ?");
-            PreparedStatement preparedStatement5 = connection.prepareStatement("DELETE FROM list_all_teacher WHERE teaching_group_number = ? AND year_of_study >= ? AND year_of_study <= ?");
+        jdbcTemplate.update("DELETE FROM list_all_student WHERE number_group = ? AND beg_stud = ? AND end_stud = ?"
+                ,group.getNumberGroup(),group.getBegStud(),group.getEndStud());
 
-            preparedStatement1.setInt(1, id);
-            ResultSet resultSet = preparedStatement1.executeQuery();
-            resultSet.next();
+        jdbcTemplate.update("DELETE FROM grade_sheet WHERE number_group = ? AND year_of_certification >= ? AND year_of_certification <= ?"
+                ,group.getNumberGroup(),group.getBegStud(),group.getEndStud());
 
-            preparedStatement5.setInt(1, resultSet.getInt("number_group"));
-            preparedStatement5.setDate(2, resultSet.getDate("beg_stud"));
-            preparedStatement5.setDate(3, resultSet.getDate("end_stud"));
-            preparedStatement5.executeUpdate();
-
-            preparedStatement4.setInt(1, resultSet.getInt("number_group"));
-            preparedStatement4.setDate(2, resultSet.getDate("beg_stud"));
-            preparedStatement4.setDate(3, resultSet.getDate("end_stud"));
-            preparedStatement4.executeUpdate();
-
-            preparedStatement3.setInt(1, resultSet.getInt("number_group"));
-            preparedStatement3.setDate(2, resultSet.getDate("beg_stud"));
-            preparedStatement3.setDate(3, resultSet.getDate("end_stud"));
-            preparedStatement3.executeUpdate();
-
-            preparedStatement2.setInt(1, id);
-            preparedStatement2.executeUpdate();
-
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
+        jdbcTemplate.update("DELETE FROM enum_of_groups WHERE id=?"
+                ,id);
     }
 }
-
